3cf141be80bcdccb4cdc5b99fa0d3621bf502abe
zahor55 <zahor55@gmail.com>
2020-06-27 21:26:22 +0300

    zahor upload

--- main.js
+++ main.js
@@ -1,64 +1,21 @@
-/* Enumerations */
+/* enum */
+//3 casting option
 const CASTING = Object.freeze({'DIAGONAL': 1, 'PARALAL': 2, 'PERSPECTIVE': 3});
+//3 axis option x y and z
 const ROTATE = Object.freeze({'X': 1, 'Y': 2, 'Z': 3});
-
-/* Global File Management Parameters */
-let isFileLoaded = false;
-
-/* Global Graphic Objects */
+let file_loaded = false;
 const vertices = [];
 const polygons = [];
 let currentCasting;
 
-function reset(reloadFile) {
-	document.getElementById('castPara').checked = 'yes';
-	document.getElementById('xAngle').value = '0';
-	document.getElementById('yAngle').value = '0';
-	document.getElementById('zAngle').value = '0';
-	document.getElementById('zoom').value = '100';
-	currentCasting = CASTING.PARALAL;
-
-	clear();
-
-	if (reloadFile === true) {
-		if (isFileLoaded === true) {
-			parseFile();
-			draw();
-		} else {
-			document.getElementById('fileInput').value = null; //clear file field for same file reload
-		}
-	} else if (reloadFile === false) {
-		isFileLoaded = false;
-	}
-}
-
-function load(fileObject) {
-	let fileList = fileObject.files;
-	if (fileList <= 0) return;
-
-	const reader = new FileReader();
-
-	reset(false);
-
-	reader.onload = () => {
-		let data = reader.result;
-
-		parseFile(data);
-		draw();
-	};
-
-	reader.readAsText(fileObject.files[0]); //Read all text in file into global variable
-}
-
-function parseFile(data) {
+function parse_file(data) {
 	if (!data) return;
 	data = data.split('\n');
 	let vertexNum = 0;
 	let polygonNum = 0;
-
 	/* Parsing - expecting 12 vertices & 10 polygons */
 	for (let line of data) {
-		// if its start with a hashtag(comment) or not a number we skip this line
+		// if its start with a #(comment) or not a number we skip this line
 		if (line[0] === '#' || Number.isNaN(line[0])) continue;
 		if (line.length < 5) continue;
 
@@ -73,26 +30,67 @@ function parseFile(data) {
 
 	if (vertexNum === 12 && polygonNum === 10) {
 		normalize();
-		isFileLoaded = true;
+		file_loaded = true;
 	} else {
-		isFileLoaded = false;
+		file_loaded = false;
 	}
 }
 
-function setCasting(cast) {
-	if (!isFileLoaded) return;
+function load(file_object) {
+	let fileList = file_object.files;
+	if (fileList <= 0) return;
+
+	const reader = new FileReader();
+
+	reset(false);
+
+	reader.onload = () => {
+		let data = reader.result;
+
+		parse_file(data);
+		draw();
+	};
+
+	reader.readAsText(file_object.files[0]);
+	//Read all text in file into global variable
+}
+
+function reset(reloadFile) {
+	document.getElementById('castPara').checked = 'yes';
+	document.getElementById('xAngle').value = '0';
+	document.getElementById('yAngle').value = '0';
+	document.getElementById('zAngle').value = '0';
+	document.getElementById('zoom').value = '100';
+	currentCasting = CASTING.PARALAL;
+
+	clear();
+
+	if (reloadFile) {
+		if (file_loaded) {
+			parse_file();
+			draw();
+		} else {
+			document.getElementById('fileInput').value = null; //clear file field for same file reload
+		}
+	} else {
+		file_loaded = false;
+	}
+}
+
+function set_casting(cast) {
+	if (!file_loaded) return;
 	currentCasting = cast;
 	draw();
 }
 
-function setAngleValue(input, axis) {
-	if (!isFileLoaded) return;
-	applyAngle(input.value, axis);
+function set_zoom_value(input) {
+	if (!file_loaded) return;
+	zoom(input.value);
 	draw();
 }
 
-function setZoomValue(input) {
-	if (!isFileLoaded) return;
-	zoom(input.value);
+function set_angle_value(input, axis) {
+	if (!file_loaded) return;
+	applyAngle(input.value, axis);
 	draw();
 }
